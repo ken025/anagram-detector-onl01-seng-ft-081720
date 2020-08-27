8c95435f7158605616688664281a7fe8324e88f2
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-27 02:21:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,8 @@ end
 def match(find_anagram_in_this_string) 
     puts "Must find #{self.word} inside the following string: #{find_anagram_in_this_string}"
     find_anagram_in_this_string.find_all do |words| 
-      if word.split("").sort == self.word.split("").sort 
-        word 
+      if words.split("").sort == self.word.split("").sort 
+        words
       end
     end 
   end
