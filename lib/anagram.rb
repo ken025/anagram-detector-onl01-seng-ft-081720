451db4d5a7a1e8551f30469bb194975ab13b5cc9
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-27 02:20:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,5 +5,12 @@ def initialize(word)
   @word = word
 end
 
-def 
+def match(find_anagram_in_this_string) 
+    puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
+    find_anagram_in_this_string.find_all do |word| 
+      if word.split("").sort == self.word_to_analyze.split("").sort 
+        word 
+      end
+    end 
+  end
 end
